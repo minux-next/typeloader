a32976add1603e436ab45e222360bb72af078660
xming <ming.xu@chip2001.com.cn>
2018-06-09 09:18:54 +0800

    添加项目文件。

---
+++ Minux.TypeLoader/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Minux.TypeLoader
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTypeLoader(this IServiceCollection services)
+        {
+            services.AddSingleton<TypeLoader>();
+            return services;
+        }
+    }
+}
---
+++ Minux.TypeLoader/TypeLoader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.Extensions.Logging;
+
+namespace Minux.TypeLoader
+{
+    public class TypeLoader
+    {
+        private readonly IServiceProvider _provider;
+        private readonly ILogger<TypeLoader> _logger;
+
+        public TypeLoader(IServiceProvider provider, ILogger<TypeLoader> logger)
+        {
+            _provider = provider;
+            _logger = logger;
+        }
+
+        public IEnumerable<Type> Load<TService>()
+        {
+            Type typeOfService = typeof(TService);
+
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (typeOfService.IsAssignableFrom(type))
+                    {
+                        yield return type;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<(Type, TAttribute)> Load<TService, TAttribute>()
+        where TAttribute : Attribute
+        {
+            Type typeOfService = typeof(TService);
+
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (typeOfService.IsAssignableFrom(type))
+                    {
+                        var attribute = type.GetCustomAttribute<TAttribute>();
+                        if (attribute != null)
+                            yield return (type, attribute);
+                    }
+                }
+            }
+        }
+    }
+}
